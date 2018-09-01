41f6a7b994886985cf3934c2834cb6fe94177cae
Matt Boisseau <boisseaumr@gmail.com>
2018-08-31 22:43:56 -0400

    Add car

---
+++ GMTK2/Assets/Car Movement/FollowObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour {
+
+	public Transform objectToFollow;
+
+	private Vector3 offset;
+
+	private void Start() {
+		offset = transform.position;
+	}
+
+	private void Update() {
+		transform.position = objectToFollow.position + offset;
+	}
+}
