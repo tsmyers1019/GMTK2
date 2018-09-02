22fde43bdc513a48f27afa81977d27fcd42a47b9
Matt Boisseau <boisseaumr@gmail.com>
2018-09-02 12:05:07 -0400

    It's a TITLE SCREEN, my dude!

---
+++ GMTK2/Assets/MainMenuUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour {
+
+	public void GoToNextScene() {
+		SceneManager.LoadScene(1);
+	}
+	
+	public void Quit() {
+		Application.Quit();
+	}
+}
