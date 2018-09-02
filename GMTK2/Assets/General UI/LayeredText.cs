ef49fb7eb62275bac9425a7d8dc13d6074f9af6c
Matt Boisseau <boisseaumr@gmail.com>
2018-09-02 14:54:44 -0400

    Add shaky text effect

--- GMTK2/Assets/General UI/LayeredText.cs
+++ GMTK2/Assets/General UI/LayeredText.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class LayeredText : MonoBehaviour {
 
+	public float jiggleInterval, jiggleAmount;
+
 	[SerializeField] private string _text;
 	public string Text {
 		get { return _text; }
@@ -14,6 +16,15 @@ public class LayeredText : MonoBehaviour {
 		}
 	}
 
+	private Dictionary<Text, Vector3> initialPositions = new Dictionary<Text, Vector3>();
+
+	private void Start() {
+		foreach(Text t in GetComponentsInChildren<Text>()) {
+			initialPositions.Add(t, t.transform.position);
+		}
+		StartCoroutine(jiggle());
+	}
+
 	private void OnDrawGizmosSelected() {
 		updateText();
 	}
@@ -21,6 +32,15 @@ public class LayeredText : MonoBehaviour {
 	private void updateText() {
 		foreach(Text t in GetComponentsInChildren<Text>()) {
 			t.text = _text;
+		}
+	}
+
+	private IEnumerator jiggle() {
+		while(true) {
+			yield return new WaitForSeconds(jiggleInterval);
+			foreach(Text t in GetComponentsInChildren<Text>()) {
+				t.transform.position = initialPositions[t] + Random.Range(-jiggleAmount, jiggleAmount) * Vector3.left;
+			}
 		}
 	}
 }
